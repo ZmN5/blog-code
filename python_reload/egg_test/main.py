cb5c6bf7bae54b04b1906710b6f110a94f4f758e
fucangyu <cangyufu@gmail.com>
2019-03-29 00:31:28 +0800

    egg test1

---
+++ python_reload/egg_test/main.py
@@ -0,0 +1,29 @@
+import sys
+from importlib import reload
+import shutil
+
+shutil.copyfile('egg_test_v1.egg', 'egg_test.egg')
+
+sys.path.append('egg_test.egg')
+
+#  --- step 1 ---
+
+import egg_test
+
+from egg_test.func import test_func
+
+print(test_func())
+
+
+# --- step 2 ---
+
+shutil.copyfile('egg_test_v2.egg', 'egg_test.egg')
+
+
+reload(egg_test)
+
+from egg_test.func import test_func
+
+print(test_func())
+
+
